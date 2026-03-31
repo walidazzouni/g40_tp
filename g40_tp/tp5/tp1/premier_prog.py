37655e04fa8966eff261f5886b506cccf75af9f8
walidazzouni <walid.azzouni2001@gmail.com>
2026-03-31 12:07:47 +0200

    Nettoyage tp5

--- g40_tp/tp5/tp1/premier_prog.py
+++
@@ -1,6 +0,0 @@
-def ecrire_message(message) : 
-    print(message)
-
-if __name__ == '__main__' :
-    ecrire_message('mon premùier script')
-
--- g40_tp/tp5/tp2/contient_doublons.py
+++
@@ -1,7 +0,0 @@
-def contient_doublons(t):
-    # Vérifie si la taille du tableau est différente de la taille de l'ensemble (qui supprime les doublons)
-    return len(t) != len(set(t))
-
-# Exemples donnés
-print(contient_doublons([1, 2, 3, 1]))  # Sortie attendue: True (car il y a un doublon)
-print(contient_doublons([1, 2, 3, 4]))  # Sortie attendue: False (aucun doublon)
